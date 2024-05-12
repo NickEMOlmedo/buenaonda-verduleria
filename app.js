ad7b517084ec9999a3ac6255b7defcbee6c830c7
AnaMateasovich <anamateasovich98@gmail.com>
2024-05-12 17:16:58 -0300

    agregue los input que faltaban en contacto

---
+++ app.js
@@ -0,0 +1,6 @@
+// const inputNombre = document.querySelector('#nombre');
+// const inputApellido = document.querySelector('#apellido');
+// const inputEmail = document.querySelector('#email');
+// const inputGenero = document.querySelector('#genero');
+// const selectMotivo = document.querySelector('#motivo');
+// const inputMensaje = document.querySelector('#mensaje');
